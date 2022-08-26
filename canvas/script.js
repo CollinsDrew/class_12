e45adcd25ab5db8a009a20f6b9aa3683bc1c4755
CollinsDrew <collinsdrew1990@gmail.com>
2022-08-26 19:45:18 +0100

    line added

--- canvas/script.js
+++ canvas/script.js
@@ -4,6 +4,12 @@ let canvas = document.getElementById("myCanvas");
 
 let ctx = canvas.getContext("2d");
 
+// rectangle
 ctx.fillStyle = "red";
 
 ctx.fillRect(0,0,150,75);
+
+//line
+ctx.moveTo(0,0);
+ctx.lineTo(200,200);
+ctx.stroke();
