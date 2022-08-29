2993a20ba2b225b7c5b1d18eef32f570085977b2
CollinsDrew <collinsdrew1990@gmail.com>
2022-08-29 20:00:20 +0100

    color generator added to charts2

--- chartsJS/charts2.js
+++ chartsJS/charts2.js
@@ -1,7 +1,22 @@
 'use strict'
 
-//step 3
+//color generator
+function colorGenerator(){
+    let red = Math.floor(Math.random()*255);
+    let green = Math.floor(Math.random()*255);
+    let blue = Math.floor(Math.random()*255);
 
+    return `rgb(${red}, ${green}, ${blue})`;
+}
+console.log(colorGenerator());
+
+const labels = ['Jan', 'Feb','Mar', 'Apr', 'May','June'];
+
+//step 3
+const data = {
+    labels: ['Jan', 'Feb','Mar', 'Apr', 'May','June'],
+
+}
 
 //step 2
 const config ={
@@ -12,6 +27,6 @@ const config ={
 
 //step 1
 const myChart = new Chart(
-    document.getElementById('myChart'),
+    document.getElementById('myChart2'),
     config
 );
