f5cded11fd646e489000968a097e86e3651218b3
CollinsDrew <collinsdrew1990@gmail.com>
2022-08-29 20:30:19 +0100

    graph added

--- chartsJS/charts2.js
+++ chartsJS/charts2.js
@@ -7,26 +7,39 @@ function colorGenerator(){
     let blue = Math.floor(Math.random()*255);
 
     return `rgb(${red}, ${green}, ${blue})`;
-}
-console.log(colorGenerator());
+};
 
-const labels = ['Jan', 'Feb','Mar', 'Apr', 'May','June'];
+const lbs = ['Jan', 'Feb','Mar', 'Apr', 'May','June'];
+
+const differentColors = [];
+
+for (let i = 0; i < lbs.length; i++){
+    differentColors.push(colorGenerator());
+};
+
+console.log(differentColors);
 
 //step 3
 const data = {
-    labels: ['Jan', 'Feb','Mar', 'Apr', 'May','June'],
-
-}
+    labels: lbs,
+    datasets: [{
+        label: 'My Class Training',
+        backgroundColor: differentColors,
+        borderColor: 'black',
+        data: [15, 10, 25, 2, 20, 30],
+}]
+};
 
 //step 2
-const config ={
+const config = {
     type: 'bar',
     data: data,
     options:{}
 };
 
+
 //step 1
 const myChart = new Chart(
-    document.getElementById('myChart2'),
+    document.getElementById("chartz"),
     config
 );
