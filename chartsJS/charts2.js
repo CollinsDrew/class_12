9b3b85ffc09824608f9716600d6407a75e6a8a4d
CollinsDrew <collinsdrew1990@gmail.com>
2022-08-29 19:47:56 +0100

    step 1 and 2 added for charts2

---
+++ chartsJS/charts2.js
@@ -0,0 +1,17 @@
+'use strict'
+
+//step 3
+
+
+//step 2
+const config ={
+    type: 'bar',
+    data: data,
+    options:{}
+};
+
+//step 1
+const myChart = new Chart(
+    document.getElementById('myChart'),
+    config
+);
