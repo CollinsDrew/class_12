9cf24d528f6a0a0a676cba3acb54d08fae3c27ec
CollinsDrew <collinsdrew1990@gmail.com>
2022-08-29 19:23:08 +0100

    color generator added

--- chartsJS/charts.js
+++ chartsJS/charts.js
@@ -1,6 +1,14 @@
 'use strict'
 
+function colorGenerator(){
+    let red = Math.floor(Math.random()*255);
+    let green = Math.floor(Math.random()*255);
+    let blue = Math.floor(Math.random()*255);
 
+    return `rgb(${red},${green},${blue})`;
+}
+
+console.log(colorGenerator());
 
 const data = {
     labels: ['a','b','c','d','e','f'],
